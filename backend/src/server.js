6f8d5ad7edffd951696de4d66b977094c3f28d99
Muhammad Umar <muhammad.umar.218980@gmail.com>
2026-03-04 00:38:28 +0500

    Enable cookie parsing and CORS in server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
+const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Server is ready");
+  res.send("This is the initial backend server for MERN-Auth application.");
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
