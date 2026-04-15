b7ca2712586ce904935136bcb963841c95c0c013
Muhammad Umar <muhammad.umar.218980@gmail.com>
2026-04-16 01:42:25 +0500

    Added auth routes and mount at /api/auth

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,10 @@
 import express from "express";
-import dotenv from "dotenv";
+import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRouter from "./routes/auth.routes.js";
 
-dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -17,6 +17,8 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("This is the initial backend server for MERN-Auth application.");
 });
+
+app.use("/api/auth", authRouter);
 
 connectDB().then(()=>{
   app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
