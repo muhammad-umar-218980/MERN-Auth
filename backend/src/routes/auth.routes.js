0e74784182a6399296a39c9c28362e88fdf07973
Muhammad Umar <muhammad.umar.218980@gmail.com>
2026-04-17 01:08:24 +0500

    Updated the Auth routes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { register, login, logout } from "../controllers/auth.controller.js";
+import { register, login, logout, sendVerifyOTP, verifyEmail, isAuthenticated , sendResetPasswordOTP ,ResetPassword } from "../controllers/auth.controller.js";
+import userAuth from "../middleware/userAuth.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
 authRouter.post("/logout", logout);
+authRouter.post("/send-verify-otp", userAuth, sendVerifyOTP);
+authRouter.post("/verify-email", userAuth, verifyEmail);
+authRouter.post("/is-authenticated", userAuth, isAuthenticated);
+authRouter.post("/send-reset-password-otp", sendResetPasswordOTP);
+authRouter.post("/reset-password", ResetPassword);
 
 export default authRouter;
